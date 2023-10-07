a28df4ac4e6c70353063e60e544916fe291ce119
supattana-s <supattana.sue@gmail.com>
2023-10-07 15:40:00 +0700

    remove all plugin by packer

--- lua/pondubu/packer.lua
+++ lua/pondubu/packer.lua
@@ -7,49 +7,49 @@ return require("packer").startup(function(use)
 	-- Packer can manage itself
 	use("wbthomason/packer.nvim")
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		-- or                            , branch = '0.1.x',
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+	--use({
+	--	"nvim-telescope/telescope.nvim",
+	--	tag = "0.1.1",
+	--	-- or                            , branch = '0.1.x',
+	--	requires = { { "nvim-lua/plenary.nvim" } },
+	--})
 
-	use("Mofiqul/dracula.nvim")
+	--use("Mofiqul/dracula.nvim")
 
-	vim.cmd([[colorscheme dracula-soft]])
+	--vim.cmd([[colorscheme dracula-soft]])
 
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	--use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-	use("mbbill/undotree")
+	--use("mbbill/undotree")
 
-	use("tpope/vim-fugitive")
+	--use("tpope/vim-fugitive")
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v2.x",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" }, -- Required
-			{ -- Optional
-				"williamboman/mason.nvim",
-				run = function()
-					pcall(vim.cmd, "MasonUpdate")
-				end,
-			},
-			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+	--use({
+	--	"VonHeikemen/lsp-zero.nvim",
+	--	branch = "v2.x",
+	--	requires = {
+	--		-- LSP Support
+	--		{ "neovim/nvim-lspconfig" }, -- Required
+	--		{ -- Optional
+	--			"williamboman/mason.nvim",
+	--			run = function()
+	--				pcall(vim.cmd, "MasonUpdate")
+	--			end,
+	--		},
+	--		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" }, -- Required
-			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
-			{ "L3MON4D3/LuaSnip" }, -- Required
-		},
-	})
+	--		-- Autocompletion
+	--		{ "hrsh7th/nvim-cmp" }, -- Required
+	--		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+	--		{ "L3MON4D3/LuaSnip" }, -- Required
+	--	},
+	--})
 
-	use("ThePrimeagen/vim-be-good")
+	--use("ThePrimeagen/vim-be-good")
 
-	use("jose-elias-alvarez/null-ls.nvim")
+	--use("jose-elias-alvarez/null-ls.nvim")
 
-	use("windwp/nvim-ts-autotag")
+	--use("windwp/nvim-ts-autotag")
 
-	use("rbong/vim-flog")
+	--use("rbong/vim-flog")
 end)
