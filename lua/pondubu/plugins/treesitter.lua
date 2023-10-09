97e77533b649da017f592a24befc243b49c4caa3
supattana-s <supattana.sue@gmail.com>
2023-10-09 14:56:10 +0700

    set up treesitter

---
+++ lua/pondubu/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "c", "lua", "go", "rust", "vim", "vimdoc", "query", "elixir", "heex", "javascript",
+                "typescript", "html" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end
+}
